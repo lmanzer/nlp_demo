60dcf9999c43888876b312f24a739522673b297b
Lianne Manzer <liannemanzer@gmail.com>
2019-03-21 16:38:09 -0400

    Add cbc urls

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,5 @@
 
-article_urls = [
+cnbc_urls = [
     'https://www.cnbc.com/2019/03/15/spielberg-and-netflixs-sarandos-meet-sparking-hope-for-oscars-truce.html',
     'https://www.cnbc.com/2019/03/15/tesla-shares-fall-5-percent-after-company-unveils-model-y.html',
     'https://www.cnbc.com/2019/03/15/arizona-will-go-after-big-tech-if-necessary-says-attorney-general.html',
@@ -7,4 +7,16 @@ article_urls = [
     'https://www.cnbc.com/2019/02/24/kraft-heinz-reviews-options-for-maxwell-house-coffee-including-sale.html',
     'https://www.cnbc.com/2019/03/08/jeff-bezos-to-end-secrecy-over-amazons-role-in-carbon-emissions.html',
     'https://www.cnbc.com/2019/03/15/dieselgate-sec-says-volkswagen-perpetrated-fraud-lied-to-investors.html',
+]
+
+
+cbc_urls = [
+    'https://www.cbc.ca/news/business/starbucks-greener-cup-1.5063861',
+    'https://www.cbc.ca/news/business/rogers-media-magazines-1.5064054',
+    'https://www.cbc.ca/news/business/budget-cmhc-home-buyers-1.5063204',
+    'https://www.cbc.ca/news/business/eu-regulators-fine-google-online-ads-1.5063806',
+    'https://www.cbc.ca/news/canada/nova-scotia/air-canada-max-8s-grounded-july-1-1.5062354',
+    'https://www.cbc.ca/news/business/shoppers-drug-mart-superstore-self-checkout-loblaw-1.5056800',
+    'https://www.cbc.ca/news/business/volkswagen-charged-with-defrauding-investors-1.5058925',
+    'https://www.cbc.ca/news/technology/facebook-instagram-outage-cause-1.5056807'
 ]
